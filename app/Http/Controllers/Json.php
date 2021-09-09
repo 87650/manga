a6cfa80c5819dd0de14ee9cb151fe3dbc1a421e3
Sasha <toma6456@gmail.com>
2021-09-09 16:30:06 +0300

    .

--- app/Http/Controllers/Json.php
+++ app/Http/Controllers/Json.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+//use Facade\FlareClient\Http\Response;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
+use Illuminate\Support\Facades\Response;
 
 
 class Json extends Controller
@@ -14,33 +18,73 @@ class Json extends Controller
     }
 
 
-    public function manga_all($limit){
-        $result = DB::select('select title from manga limit ?',[$limit]);
-        return [$result];
+    public function manga_all($limit, ResponseFactory $responseFactory){
+        try {
+            $result = DB::select('select title from manga limit ?', [$limit]);
+            //return [$result];
+           return $responseFactory->json([$result], 200, ['status'=>'200']);
+        }
+        catch (QueryException $e)
+        {
+            return $responseFactory->json([$result], 400);
+        }
     }
-    public function authors_all($limit){
-        $result = DB::select('select name from authors limit ?',[$limit]);
-        return [$result];
+    public function authors_all($limit, ResponseFactory $responseFactory){
+        try {
+            $result = DB::select('select name from authors limit ?', [$limit]);
+            // return [$result];
+            return $responseFactory->json([$result]);
+        }
+        catch(QueryException $e) {
+            return $responseFactory->json([$result], 400);
+        }
     }
-    public function manga_id($id){
-        $result = DB::select('select title from manga where id = ?',[$id]);
-        return [$result];
+    public function manga_id($id, ResponseFactory $responseFactory){
+        try {
+            $result = DB::select('select title from manga where id = ?',[$id]);
+            return $responseFactory->json([$result]);
+        }
+        catch(QueryException $e) {
+            return $responseFactory->json([$result], 400);
+        }
+
     }
-    public function authors_id($id){
-        $result = DB::select('select name from authors where id = ?',[$id]);
-        return [$result];
-    }
-    public function manga_authors($id){
-        $result = DB::select('select manga.title from manga join authors on id_authors = ?', [$id]);
-        return [$result];
-    }
-    public function manga_add(Request $request)
+    public function authors_id($id, ResponseFactory $responseFactory)
     {
-        //$type = $request->input('type');
-        $manga = $request->input('manga');
-        $authors = $request->input('authors');
-        $result = DB::select('select id from authors where name = ?', [$authors]);
-        $result1 = DB::insert('insert into manga (title,id_authors) values (?,?)', [$manga,$result[0]->id]);
+        try {
+            $result = DB::select('select name from authors where id = ?', [$id]);
+            return $responseFactory->json([$result]);
+        }
+        catch (QueryException $e)
+        {
+            return $responseFactory->json([$result], 400);
+        }
+    }
+
+    public function manga_authors($id,ResponseFactory $responseFactory){
+        try {
+            $result = DB::select('select manga.title from manga join authors on id_authors = ?', [$id]);
+            return $responseFactory->json([$result]);
+        }
+        catch (QueryException $e)
+        {
+            return $responseFactory->json([$result], 400);
+        }
+
+    }
+    public function manga_add(Request $request, ResponseFactory $responseFactory)
+    {
+        try {
+            //$type = $request->input('type');
+            $manga = $request->input('manga');
+            $authors = $request->input('authors');
+            $result = DB::select('select id from authors where name = ?', [$authors]);
+            $result1 = DB::insert('insert into manga (title,id_authors) values (?,?)', [$manga, $result[0]->id]);
+            return $responseFactory->json([$result1], 200);
+        }
+        catch (QueryException $e) {
+            return $responseFactory->json([], 400);
+        }
     }
 
 
